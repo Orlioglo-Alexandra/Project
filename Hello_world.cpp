a7165caeec0ec801ff7f6979b38d2218674ab433
Orlioglo-Alexandra <alexanda.orlioglo@gmail.com>
2024-04-07 17:30:54 +0400

    Add a function to the project that prints Fibonacci number starting with (1,1...)

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+int fib(int n) {
+	if (n == 0)
+		return 0;
+	if (n == 1 || n == 2)
+		return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
 	std::cout << "Hello, World!\n";
+	int n;
+	std::cin >> n;
+	std::cout << fib(n);
 	return 0;
 }
